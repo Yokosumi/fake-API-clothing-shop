9ef03ee9b748b4059f71d6ebae8497c75ad72179
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-12-07 14:43:05 +0100

    AppRouter created

---
+++ src/AppRouter.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { WelcomePage } from './pages/WelcomePage'
+import { ClothingPage } from './pages/ClothingPage'
+import { CartPage } from './pages/CartPage'
+
+export const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route path="/clothing" element={<ClothingPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/" element={<Navigate to="/welcome" />} />
+        </Routes>
+    )
+}
